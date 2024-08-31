18662e8ac56045f5ac8f7ea763675b30b3670179
Aaron Garcia <axyscreativeagency@gmail.com>
2024-08-31 16:10:43 -0600

    dynamic header, still needs cleaned up in the html

--- src/static/javascript/global/header.js
+++ src/static/javascript/global/header.js
@@ -12,4 +12,30 @@ if (headerBanner) {
   setTimeout(() => {
     siteHeader.classList.add("show-banner");
   }, 4200);
+
+  // temp banner message for dynamic discount date
+  const templateBannerMessage = (() => {
+    const getEndDate = () => {
+      const today = new Date();
+      const nextMonth = new Date(today.getFullYear(), today.getMonth() + 1, 1);
+      const lastDayNextMonth = new Date(
+        nextMonth.getFullYear(),
+        nextMonth.getMonth() + 1,
+        0
+      );
+
+      // Format the date as MM/DD/YYYY
+      const month = String(lastDayNextMonth.getMonth() + 1).padStart(2, "0");
+      const day = String(lastDayNextMonth.getDate()).padStart(2, "0");
+      const year = lastDayNextMonth.getFullYear();
+
+      return `${month}/${day}/${year}`;
+    };
+
+    const bannerText = `Don't miss our limited offer! 20% off all bundles! Offer ends ${getEndDate()}`;
+
+    const messageTextSpan = document.querySelectorAll(".message-text");
+
+    messageTextSpan.forEach((span) => (span.innerHTML = bannerText));
+  })();
 }
